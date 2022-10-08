e525e0b83cf04c1ad2d8b1d27492b5ff9b180af3
Starlyn Fernandez <s.fernandez@innovix.com.do>
2022-10-08 19:45:10 -0400

    added CRUD Operations with JWT

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 dotenv.config();
 
 
@@ -18,11 +19,9 @@ mongoose
 
 
 app.use(express.json());
-app.use("/api/user", userRoute);
+app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
-app.get("/home", (req, res) => {
-  res.send("datos del home");
-});
+app.use("/api/products", productRoute);
 
 
 
